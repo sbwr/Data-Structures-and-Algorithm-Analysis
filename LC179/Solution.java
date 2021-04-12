41d4ad60d8660b7a8459b69ce2f506a113dee18e
rui wu <2443652119@qq.com>
2021-04-12 16:45:35 +0800

    updated LC179

--- LC179/Solution.java
+++ LC179/Solution.java
@@ -1,34 +1,27 @@
 package LC179;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class Solution {
     public String largestNumber(int[] nums) {
-        int n = nums.length;
-        String numsToWord[] = new String[n];
-        for(int i=0;i<n;i++){
-            numsToWord[i] = String.valueOf(nums[i]);
-        }
-        //[233，23333]排序后变为[23333，233]
-        Arrays.sort(numsToWord,(a,b)->{
-            return (b+a).compareTo(a+b);
-        });
+        int len = nums.length;
+        ArrayList<String> numsAL = new ArrayList<String>();
+        for(int i = 0; i < len; i++){ numsAL.add(String.valueOf(nums[i])); }
+        Collections.sort(numsAL,(a,b)->{
+            return (b+a).compareTo(a+b);}); // sort using custom rule, used lambda expressions
+        // return numsAL.toString(); // will get String[] by this mean
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<n;i++){
-            sb.append(numsToWord[i]);
+        for(int i = 0; i < len; i++) {
+            sb.append(numsAL.get(i).toString());
         }
-        String res = sb.toString();
-        return res.charAt(0)=='0'?"0":res;
+        System.out.println(numsAL.getClass().toString());
+        String ans = sb.toString();
+        return ans.charAt(0)=='0'?"0":ans; // 若ans首位为0，则其为len个连续的0，输出0
     }
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] nums = {233, 23333, 2, 23};
+        int[] nums = {33, 34, 340, 3, 301};
         System.out.println(s.largestNumber(nums));
     }
 }
-
-
-// 作者：wang-qiang-ustb
-// 链接：https://leetcode-cn.com/problems/largest-number/solution/java-jiang-shu-zu-zhuan-hua-wei-zi-fu-ch-ikrv/
-// 来源：力扣（LeetCode）
-// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
