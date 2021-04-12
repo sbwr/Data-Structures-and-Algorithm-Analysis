33ec5101c742e50a66e11dfee577217bb3545e11
rui wu <2443652119@qq.com>
2021-04-12 23:34:16 +0800

    upgrated LC179

--- LC179/Solution.java
+++ LC179/Solution.java
@@ -2,23 +2,35 @@ package LC179;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 class Solution {
+    
     public String largestNumber(int[] nums) {
         int len = nums.length;
         ArrayList<String> numsAL = new ArrayList<String>();
         for(int i = 0; i < len; i++){ numsAL.add(String.valueOf(nums[i])); }
-        Collections.sort(numsAL,(a,b)->{
-            return (b+a).compareTo(a+b);}); // sort using custom rule, used lambda expressions
-        // return numsAL.toString(); // will get String[] by this mean
+
+        /*** sort using custom rule ***/
+        // Collections.sort(numsAL, new Comparator<String>(){ // assigned using anonymous class
+        //     @Override
+        //     public int compare(String a, String b) {
+        //         return (b+a).compareTo(a+b);
+        //     }
+        // });
+        Collections.sort(numsAL,(a,b)->{ // assigned by lambda expressions
+            return (b+a).compareTo(a+b);}); 
+
+        /*** ArrayList -> String, then output ***/
+        // return numsAL.toString); // will get String[] by this mean
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < len; i++) {
             sb.append(numsAL.get(i).toString());
         }
-        System.out.println(numsAL.getClass().toString());
         String ans = sb.toString();
         return ans.charAt(0)=='0'?"0":ans; // 若ans首位为0，则其为len个连续的0，输出0
     }
+
     public static void main(String[] args) {
         Solution s = new Solution();
         int[] nums = {33, 34, 340, 3, 301};
