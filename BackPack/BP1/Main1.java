e83c4f36d53eee80f069b1d7ec0102e9e5fe15df
sbwr <18600240302>
2021-04-22 22:02:04 +0800

    更新 背包问题

---
+++ BackPack/BP1/Main1.java
@@ -0,0 +1,42 @@
+package BackPack.BP1;
+
+import java.util.Scanner;
+
+public class Main1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt(), V = in.nextInt();
+        int[] v = new int[N], w = new int[N];
+        for(int i = 0; i < N; i++) {
+            v[i] = in.nextInt();
+            w[i] = in.nextInt();
+        }
+        in.close();
+        System.out.println(maxValue(N, V, v, w));
+    }
+    private static int maxValue(int N, int V, int[] v, int[] w){
+        if (N == 0 || V == 0) return 0;
+        int[] dp = new int[V+1];
+        for (int i = 0; i < N; i++){ // 外循环所有物品，从0开始递增
+            for (int j = V; j >= v[i]; j--){ // 内循环每一个剩余空间量，从V开始递减
+                dp[j] = Math.max(dp[j], dp[j-v[i]] + w[i]);
+            }
+        }
+        return dp[V];
+    }
+    private static int maxValue2(int N, int V, int[] v, int[] w) {
+        if (N == 0 || V == 0) return 0;
+        int[][] dp = new int[N][V+1];
+        for(int i = 0; i < N; i++){
+            for(int j = v[i]; j <= V; j++){
+                if(j == 0) continue;
+                if(i == 0) dp[i][j] = w[i];
+                // 选择装或不装物品i
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-v[i]] + w[i]);
+                }
+            }
+        }
+        return dp[N-1][V];
+    }
+}
--- LC91/Solution.java
+++ LC91/Solution.java
@@ -2,25 +2,25 @@ package LC91;
 
 class Solution {
     public int numDecodings(String s) {
-        char[] c = s.toCharArray();
-        int i = 0, len = c.length;
+        // char[] c = s.toCharArray(); // 使用额外空间，避免频繁使用charAt
+        int i = 0, len = s.length();
         int[] record = new int[3]; // 循环记录[i-2, i]处累计的解码方式数量
         // 单独处理i=0，i=1；此处冗长复杂，待优化
-        if(c[i] == '0') return 0;
+        if(s.charAt(i) == '0') return 0;
         else {
             record[0] = 1;
             i++;
         }
         if(len == 1) return 1;
-        if(c[i] != '0') record[1] += record[0];
-        if(c[i-1] == '1' || c[i-1] == '2' && c[i] >= '0' && c[i] <= '6'){ // 可组成两位码
+        if(s.charAt(i) != '0') record[1] += record[0];
+        if(s.charAt(i-1) == '1' || s.charAt(i-1) == '2' && s.charAt(i) >= '0' && s.charAt(i) <= '6'){ // 可组成两位码
             record[1] ++;
         }
         i++;
         for(; i < len; i++){
             record[i%3] = 0;
-            if(c[i] != '0') record[i%3] += record[(i-1)%3];
-            if(c[i-1] == '1' || c[i-1] == '2' && c[i] >= '0' && c[i] <= '6'){ // 可组成两位码
+            if(s.charAt(i) != '0') record[i%3] += record[(i-1)%3];
+            if(s.charAt(i-1) == '1' || s.charAt(i-1) == '2' && s.charAt(i) >= '0' && s.charAt(i) <= '6'){ // 可组成两位码
                 record[i%3] += record[(i-2)%3];
             }
         }
