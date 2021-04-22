2c4628c491f684596e88671d97a451fbb3935c78
sbwr <18600240302>
2021-04-23 00:20:35 +0800

    updated 背包

--- BackPack/BP2/Main.java
+++ BackPack/BP2/Main.java
@@ -19,23 +19,14 @@ public class Main {
         int[][] dp = new int[N+1][V+1];
         for(int i = 1; i <= N; i++){
             for(int j = 1; j <= V; j++){ // j=0时dp全为0，j=1时开始考虑
-                // if(i == 0) {
-                //     if(j < v[i]) {
-                //         dp[i][j] = w[i];
-                //     }
-                //     else {
-                //         dp[i][j] = dp[i][j - v[i]] + w[i];
-                //     }
-                // }
-                // // 选择装或不装物品i
-                // else{
-                    if(j < v[i-1])
-                        dp[i][j] = dp[i-1][j];
-                    else
-                        dp[i][j] = Math.max(dp[i-1][j], dp[i][j-v[i-1]] + w[i-1]);
-                // }
+                if(j < v[i-1]){
+                    dp[i][j] = dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-v[i-1]] + w[i-1]);
+                }
             }
         }
-        return dp[N-1][V];
+        return dp[N][V];
     }
 }
